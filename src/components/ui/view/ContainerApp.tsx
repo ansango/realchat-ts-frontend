10c4658c9f7f076f2a542558a808c9b96a9f4c2b
ansango <anibalsantosgo@gmail.com>
2021-09-02 20:46:51 +0200

    props

--- src/components/ui/view/ContainerApp.tsx
+++ src/components/ui/view/ContainerApp.tsx
@@ -1,6 +1,10 @@
 import ContainerTheme from "./ContainerTheme";
 
-export const ContainerApp = ({ children }: any) => {
+type ContainerProps = {
+  children: React.ReactElement;
+};
+
+export const ContainerApp = ({ children }: ContainerProps) => {
   return (
     <ContainerTheme>
       <div className="min-h-screen flex flex-col max-w-3xl px-4 mx-auto sm:px-6 xl:max-w-5xl xl:px-0">
--- src/components/ui/view/ContainerTheme.tsx
+++ src/components/ui/view/ContainerTheme.tsx
@@ -1,4 +1,7 @@
-const ContainerTheme = ({ children }: any) => {
+type ContainerProps = {
+  children: React.ReactElement;
+};
+const ContainerTheme = ({ children }: ContainerProps) => {
   return (
     <div className="bg-white dark:bg-gray-900 text-blue-900 dark:text-blue-200 ">
       {children}
